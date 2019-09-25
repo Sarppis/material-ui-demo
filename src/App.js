15833db5774404e2d43723813735bff48d998712
SannaSarpaneva <Sarppis@users.noreply.github.com>
2019-09-25 14:18:25 +0300

    my version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import ButtonAppBar from './ButtonAppBar';
+import Grid from './Grid';
+import Cards from './Cards';
+import Form from './Form';
+import Button from './Button';
+import List from './List';
+import Radio from './Radio';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ButtonAppBar />
+      <Grid />
+      <Form />
+      <Button />
+      <List />
+      <Radio />
     </div>
+
   );
 }
 
